df34afb57104f5b28f827eb21d6d96e8372114b8
Michal Flak <michal@betsoldsport.com>
2018-11-24 19:16:42 +0100

    przykladowy commit

---
+++ FakeU/FakeUTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FakeUAgent;
+
+namespace FakeUTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+            //zmiana
+        }
+    }
+}
