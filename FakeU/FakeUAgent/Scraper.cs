448c93c52e2d3984238cddaa641194b1193059e9
Nehhaket <nehhaket@gmail.com>
2018-11-24 21:16:27 +0100

    Add Scraper.cs class template

---
+++ FakeU/FakeUAgent/Scraper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FakeUAgent
+{
+    class Scraper
+    {
+        public Listing Scrape(Uri uri)
+        {
+            var listing = new Listing();
+
+
+
+            return listing;
+        }
+    }
+}
